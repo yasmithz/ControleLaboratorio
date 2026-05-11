9424e98f546312fe18298db14a2ce2dc495c92b9
yasmin smith <yasescola680@gmail.com>
2026-05-10 23:15:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
             System.out.println("TESTE 1: Verificando equipamentos DISPONÍVEIS no estoque...");
             List<Equipamento> disponiveis = equipamentoDAO.listarDisponiveis();
             for (Equipamento eq : disponiveis) {
-                System.out.println("   📦 ID: " + eq.getId() + " | " + eq.getNome());
+                System.out.println("    ID: " + eq.getId() + " | " + eq.getNome());
             }
             System.out.println("---------------------------------------------------------");
 
@@ -53,9 +53,9 @@ public class Main {
             emprestimoDAO.listarHistorico();
             System.out.println("---------------------------------------------------------");
 
-            System.out.println("\n=========================================================");
-            System.out.println(" 🎉 APRESENTAÇÃO FINALIZADA COM SUCESSO! NOTA 10! 🎉 ");
-            System.out.println("=========================================================");
+            System.out.println("\n===================================================");
+            System.out.println("  APRESENTAÇÃO FINALIZADA COM SUCESSO!  ");
+            System.out.println("===================================================");
 
         } catch (Exception e) {
             System.err.println(" Ocorreu um erro durante a apresentação: " + e.getMessage());
